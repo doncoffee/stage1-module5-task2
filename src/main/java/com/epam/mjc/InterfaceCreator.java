77ba266be6928abf34f79fbb10b9b4154bd1f206
Ilya <iliaabr5@gmail.com>
2023-02-01 09:44:05 +0300

    add decision

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,18 @@
 package com.epam.mjc;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return new Operation<>() {
+            @Override
+            public List<Integer> apply(List<Integer> arg) {
+                return arg.stream()
+                        .map(i -> i / divider)
+                        .collect(Collectors.toList());
+            }
+        };
     }
 }
